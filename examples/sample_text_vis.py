06d26d720570d4ad16eaa5a3f0efcb6d853895cd
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2018-05-08 23:09:58 +0200

    more interesting examples with trump tweets

--- examples/sample_text_vis.py
+++ examples/sample_text_vis.py
@@ -1,8 +1,8 @@
+import pandas as pd
+
 from embedisualization.embedisualization import Embedisualisation
 
 if __name__ == '__main__':
-    emb = Embedisualisation([
-        'This is example. This is subfirst.', 'this is next example', 'this is third example',
-        'This is task. This is subfirst.', 'this is next tesk', 'this is third tesk',
-    ])
+    trump_tweets_df = pd.read_csv('realDonaldTrump_tweets.csv')
+    emb = Embedisualisation(trump_tweets_df.dropna().text.tolist())
     emb.create_d3_visualisation()
