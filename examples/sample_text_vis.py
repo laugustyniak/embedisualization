e9ccf2658cfc5daaf07ad703bb5d423d99fd5886
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2018-05-09 01:18:40 +0200

    sample 1000 tweets

--- examples/sample_text_vis.py
+++ examples/sample_text_vis.py
@@ -4,5 +4,7 @@ from embedisualization.embedisualization import Embedisualisation
 
 if __name__ == '__main__':
     trump_tweets_df = pd.read_csv('realDonaldTrump_tweets.csv')
-    emb = Embedisualisation(trump_tweets_df.dropna().text.tolist())
+    # filter some tweets
+    trump_tweets_df = trump_tweets_df[trump_tweets_df.text.str.len() > 30].sample(1000)
+    emb = Embedisualisation(trump_tweets_df.text.tolist())
     emb.create_d3_visualisation()
